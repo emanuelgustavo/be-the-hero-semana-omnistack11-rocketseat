440fda5b54eda4c4e52943862228c70aa66809e5
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2020-04-06 22:19:17 -0300

    regra de negocio do search atualizada

--- Back-End/src/controllers/searchController.js
+++ Back-End/src/controllers/searchController.js
@@ -20,9 +20,18 @@ module.exports = {
       return incident.incident_id;
     });
     //busca os incidents que não estão na tabela de histórico
+    /**
+     * No search de incidents, aparece apenas os casos abertos
+     * e dentro do prazo. Se o caso recebe uma doação, ele passa
+     * a ter o status de resolvido e sai do search. Se o caso
+     * passa do deadline de arrecadação sem receber doação
+     * ele passa a ter o status de não resolvido e sai do search
+     * passando a aparecer para a ong como não resolvido.
+     */
     const incidents = await connection('incidents')
       .join('ongs', function () {
         this.on('ongs.id', '=', 'incidents.ong_id')
+          .onIn('incidents.status', ['aberto', 'Aberto'])
           .onNotIn('incidents.id', incidentIdOnHistory);
       })
       .select([
